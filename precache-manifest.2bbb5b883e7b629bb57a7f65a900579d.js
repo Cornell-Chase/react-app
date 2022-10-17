523dc9ad8b6de66c2af711cf57b699d876cd1863
Cornell-Chase <72884433+Cornell-Chase@users.noreply.github.com>
2022-10-17 14:10:57 +0000

    first-react-app

---
+++ precache-manifest.2bbb5b883e7b629bb57a7f65a900579d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24157fa6fb7dda579ade",
+    "url": "/static/css/main.c2d7ffca.chunk.css"
+  },
+  {
+    "revision": "24157fa6fb7dda579ade",
+    "url": "/static/js/main.24157fa6.chunk.js"
+  },
+  {
+    "revision": "9fd5143e2723a46e586c",
+    "url": "/static/js/1.9fd5143e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d76fd95e17fca58f55f9745ce4d027fa",
+    "url": "/static/media/code.jpg.d76fd95e.jpg"
+  },
+  {
+    "revision": "5a1b28bdda9e75788f8ce1b93bbcd7b3",
+    "url": "/index.html"
+  }
+];
